391dfd64bed578f868bea312c0a522cbb801734f
Henning Jansen <henning.jansen@jansenh.no>
2025-03-17 09:57:02 +0100

    Add clj-go.core with basic functions for accessing position on board
    - Includes tests

---
+++ src/no/jansenh/clj_go/core.clj
@@ -0,0 +1,27 @@
+(ns no.jansenh.clj-go.core
+  (:require [no.jansenh.clj-go.board :refer [board-state]]))
+
+(def game-state (deref board-state))
+
+(def board (:board game-state))
+
+(defn position
+  "Gets state of a given position on board.
+   - position x is horisontal,
+   - position y is vertical."
+  [x y b]
+  (get-in b [y x]))
+
+(defn neighbours-position
+  "Gets the positions of the neighbors on a given position, arranged
+   as follows: north, east, south, and west."
+  [x y b]
+  [[x (- y 1)][(+ x 1) y][x (+ y 1)][(- x 1) y]])
+
+(defn neighbours-value
+  "Gets the values of the neighbors on a given position, arranged
+   as follows: north, east, south, and west."
+  [x y b]
+  (let [neighbours (neighbours-position x y b)]
+    (into [] (map #(position (first %) (second %) b) neighbours))))
+
