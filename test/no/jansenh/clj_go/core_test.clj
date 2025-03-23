66a5a5f260480dad0797fd8548d0fe11d9a10a54
Henning Jansen <henning.jansen@jansenh.no>
2025-03-23 22:00:22 +0100

    Update core-test with test-data

--- test/no/jansenh/clj_go/core_test.clj
+++ test/no/jansenh/clj_go/core_test.clj
@@ -2,20 +2,31 @@
   (:require [clojure.test :refer [deftest is testing]]
             [no.jansenh.clj-go.core :as sut]))
 
-(def numeric-board [[1   2   3   4   5   6   7   8   9   10  11  12  13]
-                    [14  15  16  17  18  19  20  21  22  23  24  25  26]
-                    [27  28  29  30  31  32  33  34  35  36  37  38  39]
-                    [40  41  42  43  44  45  46  47  48  49  50  51  52]
-                    [53  54  55  56  57  58  59  60  61  62  63  64  65]
-                    [66  67  68  69  70  71  72  73  74  75  76  77  78]
-                    [79  80  81  82  83  84  85  86  87  88  89  90  91]
-                    [92  93  94  95  96  97  98  99  100 101 102 103 104]
-                    [105 106 107 108 109 110 111 112 113 114 115 116 117]
-                    [118 119 120 121 122 123 124 125 126 127 128 129 130]
-                    [131 132 133 134 135 136 137 138 139 140 141 142 143]
-                    [144 145 146 147 148 149 150 151 152 153 154 155 156]
-                    [157 158 159 160 161 162 163 164 165 166 167 168 169]])
+;;; ----------------------------------------------------------------------------
+;;;   Test data
+;;;
 
+;; We got a data-structure with numbers only, used for 
+;; testing vectors and positions. The data-structure is
+;; a representation of a board with 13 by 13 vector,
+;; numbers 1 to 169.
+(def numeric-board
+  (vec (for [row (range 13)]
+         (vec (for [col (range 13)]
+                (inc (+ col (* row 13))))))))
+
+;; We got a scattered board with cycled values, evenly
+;; distributed white, black stones with empty intersects.
+;; The data-structure is a representation of a board with
+;; 13 by 13 vector with values :black :white and :nil.
+(def patterned-board
+  (->> (take 169 (cycle [:black :nil :white :nil]))
+       (partition 13)
+       (mapv vec)))
+
+;;; ----------------------------------------------------------------------------
+;;;   Unit tests
+;;;
 
 (deftest test-pos
   (testing "We expect to find the corresponding [x y] value on the numeric board."
