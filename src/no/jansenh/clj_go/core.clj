073d647e2ca95d6b34f6aa2673883d94f66df98c
Henning Jansen <henning.jansen@jansenh.no>
2025-03-30 12:45:09 +0200

    Refactor core functions to take map arguments
    - Update tests

--- src/no/jansenh/clj_go/core.clj
+++ src/no/jansenh/clj_go/core.clj
@@ -14,19 +14,54 @@
   "Gets state of a given position on board.
    - position x is horisontal,
    - position y is vertical."
-  [x y b]
-  (get-in b [y x]))
+  [m]
+  (let [x (:x-pos m)
+        y (:y-pos m)
+        b (:board m)]
+    (get-in b [y x])))
 
 (defn neighbours-position
   "Gets the positions of the neighbors on a given position, arranged
    as follows: north, east, south, and west."
-  [x y b]
-  [[x (- y 1)][(+ x 1) y][x (+ y 1)][(- x 1) y]])
+  [m]
+  (let [x (:x-pos m)
+        y (:y-pos m)]
+    [[x (- y 1)][(+ x 1) y][x (+ y 1)][(- x 1) y]]))
 
 (defn neighbours-value
   "Gets the values of the neighbors on a given position, arranged
    as follows: north, east, south, and west."
-  [x y b]
-  (let [neighbours (neighbours-position x y b)]
-    (into [] (map #(position (first %) (second %) b) neighbours))))
+  [m]
+  (let [x (:x-pos m)
+        y (:y-pos m)
+        b (:board m)]
+    (let [neighbours (neighbours-position (move x y b))]
+     (into [] (map #(position {:x-pos (first %) :y-pos (second %) :board b}) neighbours)))))
+
+(defn move
+  "Takes a vector of positional arguments and return map with structured argument.
+   Multiple telescopic arity, positions are x, y, player and board."
+  ([x-pos y-pos]
+   (let [x-pos  x-pos
+         y-pos  y-pos]
+     {:x-pos    x-pos
+      :y-pos    y-pos}))
+
+  ([x-pos y-pos board]
+   (let [x-pos  x-pos
+         y-pos  y-pos
+         board  board]
+     {:x-pos    x-pos
+      :y-pos    y-pos
+      :board    board}))
+
+  ([x-pos y-pos player board]
+   (let [x-pos  x-pos
+         y-pos  y-pos
+         player player
+         board  board]
+     {:x-pos    x-pos
+      :y-pos    y-pos
+      :player   player
+      :board    board})))
 
--- test/no/jansenh/clj_go/core_test.clj
+++ test/no/jansenh/clj_go/core_test.clj
@@ -30,30 +30,36 @@
 
 (deftest test-pos
   (testing "We expect to find the corresponding [x y] value on the numeric board."
-    (is (= 29 (sut/position 2 2 numeric-board)))
-    (is (= 30 (sut/position 3 2 numeric-board)))
-    (is (= 37 (sut/position 10 2 numeric-board)))
-    (is (= 141 (sut/position 10 10 numeric-board)))
-    (is (= 133 (sut/position 2 10 numeric-board)))))
+    (is (= 29 (sut/position  {:x-pos 2 :y-pos 2 :board numeric-board})))
+    (is (= 30 (sut/position  {:x-pos 3 :y-pos 2 :board numeric-board})))
+    (is (= 37 (sut/position  {:x-pos 10 :y-pos 2 :board numeric-board})))
+    (is (= 141 (sut/position {:x-pos 10 :y-pos 10 :board numeric-board})))
+    (is (= 133 (sut/position {:x-pos 2 :y-pos 10 :board numeric-board})))))
 
-(deftest test-pos
+(deftest test-pos-edges
   (testing "We expect to find the corresponding [x y] value on the numeric board edges ."
-    (is (= 1 (sut/position 0 0 numeric-board)))
-    (is (= 13 (sut/position 12 0 numeric-board)))
-    (is (= 169 (sut/position 12 12 numeric-board)))
-    (is (= 157 (sut/position 0 12 numeric-board)))))
+    (is (= 1 (sut/position (sut/move 0 0 numeric-board))))
+    (is (= 13 (sut/position (sut/move 12 0 numeric-board))))
+    (is (= 169 (sut/position (sut/move 12 12 numeric-board))))
+    (is (= 157 (sut/position (sut/move 0 12 numeric-board))))))
 
 (deftest test-neighbours-position
   (testing "We expect to fint the neighbours positions on the numeric board."
-    (is (= [[2 1] [3 2] [2 3] [1 2]] (sut/neighbours-position 2 2 numeric-board)))
-    (is (= [[10 1] [11 2] [10 3] [9 2]] (sut/neighbours-position 10 2 numeric-board)))
-    (is (= [[10 9] [11 10] [10 11] [9 10]] (sut/neighbours-position 10 10 numeric-board)))
-    (is (= [[2 9] [3 10] [2 11] [1 10]] (sut/neighbours-position 2 10 numeric-board)))))
+    (is (= [[2 1] [3 2] [2 3] [1 2]] (sut/neighbours-position (sut/move 2 2 numeric-board))))
+    (is (= [[10 1] [11 2] [10 3] [9 2]] (sut/neighbours-position (sut/move 10 2 numeric-board))))
+    (is (= [[10 9] [11 10] [10 11] [9 10]] (sut/neighbours-position (sut/move 10 10 numeric-board))))
+    (is (= [[2 9] [3 10] [2 11] [1 10]] (sut/neighbours-position (sut/move 2 10 numeric-board))))))
 
 (deftest test-neighbours-value
   (testing "We expect to find the neighbours values on the numeric board."
-    (is (= [nil 2 14 nil] (sut/neighbours-value 0 0 numeric-board)))
-    (is (= [nil nil 26 12] (sut/neighbours-value 12 0 numeric-board)))
-    (is (= [156 nil nil 168] (sut/neighbours-value 12 12 numeric-board)))
-    (is (= [144 158 nil nil] (sut/neighbours-value 0 12 numeric-board)))))
+    (is (= [nil 2 14 nil] (sut/neighbours-value (sut/move 0 0 numeric-board))))
+    (is (= [nil nil 26 12] (sut/neighbours-value (sut/move 12 0 numeric-board))))
+    (is (= [156 nil nil 168] (sut/neighbours-value (sut/move 12 12 numeric-board))))
+    (is (= [144 158 nil nil] (sut/neighbours-value (sut/move 0 12 numeric-board))))))
 
+(deftest test-move
+  (testing "We are creating move instructions via structured argument"
+    (is (= :black (:player (sut/move 1 2 :black numeric-board))))
+    (is (= :white (:player (sut/move 3 4 :white numeric-board))))
+    (is (=  3 (:x-pos (sut/move 3 4 numeric-board))))
+    (is (=  4 (:y-pos (sut/move 3 4 numeric-board))))))
