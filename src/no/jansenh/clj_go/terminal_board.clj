9e777d0fbb26c69ec72b2072f35352debdf4f07d
Henning Jansen <jansenh@jansenh-xps13.jansenh.no>
2025-04-30 22:14:19 +0200

    Add terminal-board function
    - Provides a mean for printing game state board to REPL

--- src/no/jansenh/clj_go/terminal_board.clj
+++ src/no/jansenh/clj_go/terminal_board.clj
@@ -1,20 +1,25 @@
-(ns no.jansenh.clj-go.terminal-board
-  (:require [clojure.pprint :as pp]))
+(ns no.jansenh.clj-go.terminal-board)
 
-(defn- stsp
-  ;; Keyword to symbol
-  [s]
-  (condp = s
-    :white "o"
-    :black "x"
-    :nil "-"
-    :else nil))
+(defn value-to-symbol
+  "Convert a board value to its symbolic representation."
+  [value]
+  (case value
+    :white "○ "
+    :black "● "
+    nil "  "))
 
-(defn- convert-to-symbols [v]
-  (for [r v]
-    (apply str (interpose " " (map stsp r)))))
+(defn convert-to-symbols
+  "Convert each value in the board to its symbolic representation."
+  [board]
+  (map (fn [row]
+         (apply str (map value-to-symbol row)))
+       board))
 
 (defn symbolic-board
-  "Create a symbolic board 'o - x' for white, blank black values."
+  "Create a symbolic board 'o - ●' for white, blank (nil) and  black values.
+   The intended use is for visualization for debugging in repl or writing
+   as text file."
   [board]
   (apply str (interpose "\n" (convert-to-symbols board))))
+
+
